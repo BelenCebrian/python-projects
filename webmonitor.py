f2894e912bf0461672966bee868c5c34b9f1b351
belen cebrian <bcs.dreamer@gmail.com>
2020-09-27 16:21:35 +0200

    fix 'Message too long' in telegram method

--- webmonitor.py
+++ webmonitor.py
@@ -69,7 +69,7 @@ def android():
     # dia de lanzamiento como fecha, para luego cambiarle el formato
     fecha = datetime.datetime.strptime(dia, "%Y-%m-%d")
     version = "Android-x86: "+texto+" [{:%d %b. %Y}]".format(fecha)
-    #f"Android-x86: {texto} {dia:%d/%m/%Y}"
+    # f"Android-x86: {texto} {dia:%d/%m/%Y}"
     return version
 
 
@@ -79,45 +79,37 @@ def asus():
     url2 = "https://www.asus.com/support/api/product.asmx/GetPDBIOS?website=es&pdhashedid=svItyTHFccLwnprr&model=RT-AC68U&cpu=&callback=supportpdpage"
     s = requests.session()
     r = s.get(url2, headers=H1)
-    limpio = (r.text.strip('supportpdpage(')).replace('S"})', 'S"}')
+    clean = (r.text.strip('supportpdpage(')).replace('S"})', 'S"}')
 
-#    print(formato)
-    datos = json.loads(limpio)
-#    print(datos)
-#    print(json.dumps(datos, indent=4))
-#    print(json.dumps(datos, sort_keys=True, indent=4))
-#    print(datos.keys()) #dict_keys(['Result', 'Status', 'Message'])
+    data = json.loads(clean)
 
-    with open("response_formato.txt", 'w') as f1, open("response_original.txt", 'w') as f2:
-        print(limpio, file=f1)
+    with open("response_formated.txt", 'w') as f1, open("response_original.txt", 'w') as f2:
+        print(clean, file=f1)
         print(r.text, file=f2)
 
     # leemos del archivo guardado
-#    with open("response_formato.json", "r") as archivo:
-#        datos = json.load(archivo)
+    # with open("response_formated.json", "r") as file:
+    #    data = json.load(file)
+    # print(data['Result'].keys()) #dict_keys(['Count', 'Obj'])
 
-    # print(datos['Result'].keys()) #dict_keys(['Count', 'Obj'])
+    firmware = data['Result']['Obj'][0]['Files'][0]
 
-    firmware = datos['Result']['Obj'][0]['Files'][0]
+    title = (firmware['Title']).replace(" Firmware version", ":")
+    version = firmware['Version']
+    description = firmware['Description']
+    day = firmware['ReleaseDate']
+    date = datetime.datetime.strptime(day, "%Y/%m/%d")
+    size = firmware['FileSize']
+    link = firmware['DownloadUrl']['Global']
 
-    titulo = (datos['Result']['Obj'][0]['Files'][0]
-              ['Title']).replace(" Firmware version", ":")
-
-    version = datos['Result']['Obj'][0]['Files'][0]['Version']
-    descripcion = datos['Result']['Obj'][0]['Files'][0]['Description']
-    dia = datos['Result']['Obj'][0]['Files'][0]['ReleaseDate']
-    fecha = datetime.datetime.strptime(dia, "%Y/%m/%d")
-    tam = datos['Result']['Obj'][0]['Files'][0]['FileSize']
-    enlace = datos['Result']['Obj'][0]['Files'][0]['DownloadUrl']['Global']
     # print(firmware)
-
-    # soup = bs(descripcion, features="lxml")  #make BeautifulSoup
+    # soup = bs(description, features="lxml")  #make BeautifulSoup
     # prettyHTML = soup.prettify()   #prettify the html
 
-    desc = html2text.html2text(descripcion)
-    # print('\n',titulo,'\n\t',fecha,'\t',tam,'\n\t',enlace,'\n\n',desc)
-    texto = titulo+'\n\t[{:%d %b. %Y}]'.format(fecha)+'    '+tam+'\n\t'+enlace
-    return texto
+    desc = html2text.html2text(description)
+    # print('\n',title,'\n\t',date,'\t',tam,'\n\t',enlace,'\n\n',desc)
+    text = title+'\n\t[{:%d %b. %Y}]'.format(date)+'    '+size+'\n\n\t'+textwrap.indent(desc, '  \t')+'\t'+link
+    return text
 
 
 def calibre():
@@ -126,8 +118,8 @@ def calibre():
     soup = req(url)
     version = soup.find_all('h2')[0].string
     return version.replace("Release", "Calibre").replace(",", ".")
-    #ver = re.sub("[\(\[].*?[\)\]]", "", version)
-    #ver = re.sub("[^\d\.]", "", ver)
+    # ver = re.sub("[\(\[].*?[\)\]]", "", version)
+    # ver = re.sub("[^\d\.]", "", ver)
     # return ver
 
 
@@ -135,8 +127,8 @@ def comprobar_ficheros(f1, f2):
     # f1: se supone el nuevo generado por bajar_fichero(versiones.txt)
     # f2: se supone el anterior (b-versiones.txt)
 
-    #print('Comprobando versiones...')
-    #telegram('Comprobando versiones...')
+    # print('Comprobando versiones...')
+    # telegram('Comprobando versiones...')
 
     file1 = pathlib.Path(f1)
     file2 = pathlib.Path(f2)
@@ -164,7 +156,7 @@ def comprobar_ficheros(f1, f2):
 
     else:
         # f1 no existe
-        print('ERROR: no existe ', f1, '\n\tBajamos datos y recomprobamos...')
+        print('ERROR: no existe ', f1, '\n\tBajamos data y recomprobamos...')
         bajar_fichero()
         comprobar_ficheros(F1, F2)
 
@@ -172,10 +164,10 @@ def comprobar_ficheros(f1, f2):
 def comprobar_cambios(f1, f2):
 
     if filecmp.cmp(f1, f2, shallow=False):
-        #print('No hay versiones nuevas')
+        # print('No hay versiones nuevas')
         telegram('No hay versiones nuevas')
     else:
-        #print('HAY CAMBIOS!')
+        # print('HAY CAMBIOS!')
         telegram('HAY CAMBIOS!')
 
         with open(f1, 'r', encoding='utf-8') as f1, open(f2, 'r', encoding='utf-8') as f2:
@@ -188,7 +180,7 @@ def comprobar_cambios(f1, f2):
 
 
 def get_hops(url):
-    # NO USADO: para seguir redirecciones web (ASUS)
+    # NOT IN USE: for web redirections (ASUS)
 
     redirect_re = re.compile('<meta[^>]*?url=(.*?)["\']', re.IGNORECASE)
     hops = []
@@ -222,18 +214,19 @@ def github(proyect):
     url = "https://github.com/" + proyect + "/releases/latest"
     # print(url)
     soup = req(url)
-    salida = " "
+    output = " "
 
     try:
 
         version = soup.find(
             class_="f1 flex-auto min-width-0 text-normal").text.lstrip().rstrip()
 
+        # TAGS: same or less info that VERSION
         tags = (soup.find(class_="css-truncate-target",
                           attrs={'style': "max-width: 125px"}).text).lstrip()
 
         dia = soup.find("relative-time", class_="no-wrap").text
-        fecha = datetime.datetime.strptime(
+        date = datetime.datetime.strptime(
             dia, "%b %d, %Y").strftime("%d %b. %Y")
 
         body = soup.find("div", class_="markdown-body").text[:250] + "..."
@@ -241,10 +234,10 @@ def github(proyect):
         links = soup.find_all(
             "div", class_="d-flex flex-justify-between flex-items-center py-1 py-md-2 Box-body px-2")
 
-        salida = '\n\n[X] {0} {1} [{2}] {3} \n {4} \n \t{5}'.format(
-            proyect, tags, fecha, version, url, textwrap.indent(body, '  \t-'))
+        output = '\n\n[X] {0} [{1}]: {2} \n {3} \n \t{4}'.format(
+            proyect, date, version, url, textwrap.indent(body, '  \t-'))
 
-        #salida = "===",links,"==="
+        # output = "===",links,"==="
 
         for asset in links:
             t = asset.find("a").text.lstrip().rstrip()
@@ -255,43 +248,42 @@ def github(proyect):
                       "armhf", "amd64", "android", "kobo", "exe", "windows"]
 
             if any(x in t for x in filter):
-                #print("filtramos resultados...")
-                print('\n\n\t * {0} ({1}): github.com{2}'
-                      .format(t, size, download))
+                # print("filtramos resultados...")
+                # print('\n\n\t * {0} ({1}): github.com{2}'.format(t, size, download))
 
-                salida += '\n\n\t * {0} ({1}): github.com{2}'.format(t, size, download)
+                output += '\n\n\t * {0} ({1}): github.com{2}'.format(t,
+                                         size, download)
 
-            else:
-                #print("Saltamos descarga")
-                # print('\n\n\t * {0} ({1}): github.com{2}''.format(t, size, download))
+            # print("Saltamos descarga")
+            # output += '\n\n\t * {0} ({1}): github.com{2}'.format(t, size, download)
 
     except AttributeError:
         pass
 
-    return salida
+    return output
 
 
 def inkscape():
-    url = "https://inkscape.org/release/"
-    soup = req(url)
-    texto = soup.find("div", id="sidecategory")
-    version = texto.h1
+    url="https://inkscape.org/release/"
+    soup=req(url)
+    text=soup.find("div", id = "sidecategory")
+    version=text.h1
     return version.text
 
 
 def kodi():
     # firetv: arm (armv7a 32bits)
-    url_android = "https://kodi.tv/download/852"
-    url_android_32 = "http://mirrors.kodi.tv/releases/android/arm/"
-    url_android_64 = "http://mirrors.kodi.tv/releases/android/arm64-v8a/"
+    url_android="https://kodi.tv/download/852"
+    url_android_32="http://mirrors.kodi.tv/releases/android/arm/"
+    url_android_64="http://mirrors.kodi.tv/releases/android/arm64-v8a/"
 
 
 def libreoffice():
-    url = "https://libreoffice.org/download/download"
-    soup = req(url)
-    texto = soup.find_all("span", class_="dl_version_number")
-    lreciente = texto[0].string
-    lestable = texto[1].string
+    url="https://libreoffice.org/download/download"
+    soup=req(url)
+    texto=soup.find_all("span", class_ = "dl_version_number")
+    lreciente=texto[0].string
+    lestable=texto[1].string
     return lreciente, lestable
 
 
@@ -307,43 +299,43 @@ def pythonv():
 
 
 def req(url):
-    html_content = requests.get(url, headers=H1).text
-    #r = requests.get(url)
-    #html_content = r.text
+    html_content=requests.get(url, headers = H1).text
+    # r = requests.get(url)
+    # html_content = r.text
     return bs(html_content, 'lxml')
 
 
 def raspbian():
-    url = "https://www.raspberrypi.org/downloads/raspbian/"
-    soup = req(url)
+    url="https://www.raspberrypi.org/downloads/raspbian/"
+    soup=req(url)
 
-    imagenes = soup.find("div", class_="image-info")
+    imagenes=soup.find("div", class_ = "image-info")
     with open("raspbian.txt", "w") as f:
-        print(imagenes, file=f)
+        print(imagenes, file = f)
 
-    f = open("raspbian.txt", "r")
-    contenido = f.read()
+    f=open("raspbian.txt", "r")
+    contenido=f.read()
 
-    #detalles = soup.find_all("div", class_="image-details")
-    #descarga = soup.find("a", class_="btn dl-zip", href=True)
-    soup = bs(contenido, 'html.parser')
-    #print('h3:', soup.div.h3.string)
-    #print('content:', soup.div.h3.contents[0])
-    titulo = soup.div.h3.string
+    # detalles = soup.find_all("div", class_="image-details")
+    # descarga = soup.find("a", class_="btn dl-zip", href=True)
+    soup=bs(contenido, 'html.parser')
+    # print('h3:', soup.div.h3.string)
+    # print('content:', soup.div.h3.contents[0])
+    titulo=soup.div.h3.string
     # titulo.replace("with desktop an recommended software") #NO FUNCIONA BIEN
-    nombre = titulo[:-37] + "(desktop + software)"
-    #dato = soup.div.div.string
-    version = soup.div.find_next('div').find_next('div').strong.string
+    nombre=titulo[:-37] + "(desktop + software)"
+    # dato = soup.div.div.string
+    version=soup.div.find_next('div').find_next('div').strong.string
 
-    dia = version.find_next('div').strong.string
-    fecha = datetime.datetime.strptime(dia, "%Y-%m-%d")
+    dia=version.find_next('div').strong.string
+    fecha=datetime.datetime.strptime(dia, "%Y-%m-%d")
 #    print('dia: ',dia,'\nfecha: [{:%d %b. %Y}]'.format(fecha))
 
-    kernel = dia.find_next('div').strong.string
-    tam = kernel.find_next('div').strong.string
-    enlace = tam.find_next('a').find_next('a').find_next('a')['href']
+    kernel=dia.find_next('div').strong.string
+    tam=kernel.find_next('div').strong.string
+    enlace=tam.find_next('a').find_next('a').find_next('a')['href']
 
-    texto = 'Raspbian:\n\t'+nombre + \
+    texto='Raspbian:\n\t'+nombre + \
         '\n\t[{:%d %b. %Y}]'.format(fecha)+'    ' + \
         tam+'    '+kernel+' kernel\n\t'+enlace
     # print(texto)
@@ -359,63 +351,75 @@ def raspbian():
 
 def restar(data):
     # using list comprehension + list slicing
-    # remove last character from list of strings
+    # remove 2 last characters from list of strings
     return data[:-2]
 
 
 def telegram(mensaje):
-    print("telegram(", mensaje, ")")
+    # print("telegram(", mensaje, ")")
 
-    UR = "https://api.telegram.org/bot{}/sendMessage"
-    URLL = UR.format(TOKEN1)
+    data = " "
+
+    # senDocument(): pdf, zip, gif 
+    # https://core.telegram.org/bots/api#sending-files
+    
+    URD = "https://api.telegram.org/bot{}/sendDocument"
+    URM = "https://api.telegram.org/bot{}/sendMessage"
+    URLL = URM.format(TOKEN1)
 
     if os.path.isfile(mensaje):
         print("\t es fichero!\n\n")
 
-        with open(mensaje, 'r', encoding='utf-8') as f:
-            contenido = f.read()
-            #print("====== contenido:====== \n",contenido)
-            r = requests.post(URLL, data={'chat_id': CHAT1, 'text': contenido})
+        with open(mensaje, 'r', encoding = 'utf-8') as f:
+            data = f.read()
+            group = data.split(sep='[X]')
+            # print("====== fichero por grupos: ====== \n", group)
+            for item in group:
+                r = requests.post(URLL, data = {'chat_id': CHAT1, 'text': item, 'disable_web_page_preview': True})
 
     else:
-        r = requests.post(URLL, data={'chat_id': CHAT1, 'text': mensaje})
+        r = requests.post(URLL, data = {'chat_id': CHAT1, 'text': mensaje})
 
         # Forma 2: curl y pasando argumento a argumento
-        #entrad = "curl -s -X POST {} -d chat_id={} -d text='{}'".format(URLL, ID, mensaje)
+        # entrad = "curl -s -X POST {} -d chat_id={} -d text='{}'".format(URLL, ID, mensaje)
         # print(entrad)
-        #args = shlex.split(entrad)
-        #p = subprocess.Popen(args)
+        # args = shlex.split(entrad)
+        # p = subprocess.Popen(args)
         # print(args)
         # print(p)
 
-    #data = json.loads(r.text)
+    # data = json.loads(r.text)
     # print(data['ok'])
+    # print(r.content)
+    # print(r.text)
+    print("fin telegram")
 
 
 def titul(url):
-    t = lxml.html.parse(url)
+    t=lxml.html.parse(url)
     return t.find(".//title").text
 
 
 def titulo(url):
-    soup = bs(urlopen(url).read().decode('utf-8'), features="lxml")
-    linea = soup.title.string.strip('Release').strip(
+    # SOLO recupera el titulo 
+    soup=bs(urlopen(url).read().decode('utf-8'), features = "lxml")
+    linea=soup.title.string.strip('Release').strip(
         'GitHub').replace(" · ", ": ")
-    resul = restar(linea).split(':')
+    resul=restar(linea).split(':')
     return resul[1]+": "+resul[0]
 
 
 def vlc():
     # NO TERMINADO
-    url_windows = "http://get.videolan.org/vlc/last/win64/"
-    url_android = "http://get.videolan.org/vlc-android/"
-    soup_windows = req(url_windows)
-    soup_android = req(url_android)
+    url_windows="http://get.videolan.org/vlc/last/win64/"
+    url_android="http://get.videolan.org/vlc-android/"
+    soup_windows=req(url_windows)
+    soup_android=req(url_android)
 
-    version = soup_windows.find_all('h2')[0].string
+    version=soup_windows.find_all('h2')[0].string
     print(version)
 
-    texto = version
+    texto=version
     return texto
 
 
@@ -423,64 +427,64 @@ def bajar_fichero():
 
     print("Comprobando en línea...\n")
 
-    with open(F1, "w", encoding="utf-8") as f:
+    with open(F1, "w", encoding = "utf-8") as f:
 
-        print("[X]", android(), file=f)
+        print("[X]", android(), file = f)
 
-        print(" ", file=f)
-        print("[X]", asus(), file=f)
+        print(" ", file = f)
+        print("[X]", asus(), file = f)
 
-        print(" ", file=f)
-        print("[X]", calibre(), file=f)
-        print("\n[X]", gimp(), file=f)
-        print("\n[X]", inkscape(), file=f)
+        print(" ", file = f)
+        print("[X]", calibre(), file = f)
+        print("\n[X]", gimp(), file = f)
+        print("\n[X]", inkscape(), file = f)
 
-        nuevo, viejo = libreoffice()
-        print("\n[X] LibreOffice:", file=f)
-        print("\tLibreOffice Fresh (Reciente): ", nuevo, file=f)
-        print("\tLibreOffice Still (Estable):  ", viejo, file=f)
+        nuevo, viejo=libreoffice()
+        print("\n[X] LibreOffice:", file = f)
+        print("\tLibreOffice Fresh (Reciente): ", nuevo, file = f)
+        print("\tLibreOffice Still (Estable):  ", viejo, file = f)
 
-        print(" ", file=f)
-        print("[X]", raspbian(), file=f)
+        print(" ", file = f)
+        print("[X]", raspbian(), file = f)
 
-        retropie = github("RetroPie/RetroPie-Setup")
-        retro = retropie.replace(" RetroPie-Setup Script ", " ").rstrip()
-        print("[X]", retro, file=f)
+        retropie=github("RetroPie/RetroPie-Setup")
+        retro=retropie.replace(" RetroPie-Setup Script ", " ").rstrip()
+        print(retro, file = f)
 
-        print(github("sumatrapdfreader/sumatrapdf"), file=f)
-        print(github("pirate/ArchiveBox"), file=f)
-        print(github("FreshRSS/FreshRSS"), file=f)
-        print(github("pi-hole/pi-hole"), file=f)
+        print(github("sumatrapdfreader/sumatrapdf"), file = f)
+        print(github("pirate/ArchiveBox"), file = f)
+        print(github("FreshRSS/FreshRSS"), file = f)
+        print(github("pi-hole/pi-hole"), file = f)
 
-        print(" ", file=f)
-        print(github("buggins/coolreader"), file=f)
-        print(github("apprenticeharper/DeDRM_tools"), file=f)
-        print(github("koreader/koreader"), file=f)
-        print(github("seblucas/cops"), file=f)
-        print(github("janeczku/calibre-web"), file=f)
+        print(" ", file = f)
+        print(github("buggins/coolreader"), file = f)
+        print(github("apprenticeharper/DeDRM_tools"), file = f)
+        print(github("koreader/koreader"), file = f)
+        print(github("seblucas/cops"), file = f)
+        print(github("janeczku/calibre-web"), file = f)
 
-        print(" ", file=f)
-        print(github("microsoft/terminal"), file=f)
-        print(github("microsoft/PowerToys"), file=f)
+        print(" ", file = f)
+        print(github("microsoft/terminal"), file = f)
+        print(github("microsoft/PowerToys"), file = f)
 
-        print(" ", file=f)
-        print(github("atom/atom"), file=f)
-        print(github("microsoft/vscode"), file=f)
-        print(github("headmelted/codebuilds"), file=f)
+        print(" ", file = f)
+        print(github("atom/atom"), file = f)
+        print(github("microsoft/vscode"), file = f)
+        print(github("headmelted/codebuilds"), file = f)
 
-        print(" ", file=f)
-        print(github("ytdl-org/youtube-dl"), file=f)
-        print(github("TeamNewPipe/NewPipe"), file=f)
-        print(github("jellyfin/jellyfin"), file=f)
+        print(" ", file = f)
+        print(github("ytdl-org/youtube-dl"), file = f)
+        print(github("TeamNewPipe/NewPipe"), file = f)
+        print(github("jellyfin/jellyfin"), file = f)
 
         f.close()
 
 
 if __name__ == "__main__":
 
-    bajar_fichero()
-    telegram(F1)
+    # bajar_fichero()
+    #telegram(F1)
 
     # print(github("microsoft/vscode"))
-    #comprobar_ficheros(F1, F2)
-    # print(inkscape())
+    comprobar_ficheros(F1, F2)
+    # telegram(F1)
