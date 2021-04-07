d0ac164e551d5bb76643bd04cbae4e3bd098c96c
belen cebrian <bcs.dreamer@gmail.com>
2021-04-07 11:41:54 +0200

    Refactor code into new functions and notify exceptions

--- files_operations/merge_pdfs.py
+++ files_operations/merge_pdfs.py
@@ -1,14 +1,15 @@
-import PyPDF2, os, sys
-import locale, struct #for __readlink() method
 
-FILES = []
+import os, sys
+import PyPDF2
+import struct, locale #for __readlink() method
+
 OUTPUT = "Result.pdf"
 
-# resolve real link to file in Windows
+# resolve absolute link to file in Windows
+# https://gist.github.com/Winand/997ed38269e899eb561991a0c663fa49
+# http://stackoverflow.com/a/28952464/1119602
 def __readLink(path):
     # print("readlink(",path,")\n")
-    # https://gist.github.com/Winand/997ed38269e899eb561991a0c663fa49
-    # http://stackoverflow.com/a/28952464/1119602
     with open(path, 'rb') as stream:
         content = stream.read()
         # skip first 20 bytes (HeaderSize and LinkCLSID)
@@ -39,40 +40,53 @@ def __readLink(path):
 # https://unix.stackexchange.com/questions/334800/python-script-output-in-the-wrong-directory-when-called-from-cron
 os.chdir(sys.path[0]) 
 
-for filename in os.listdir():
-    f = os.path.splitext(filename) # split name and extension
-    num = f[0][0:2] # check order: 01 (01_Name.lnk)
-    if f[1] == '.lnk':
-        # print("order: '{0}', name: '{1}', ext: '{2}' \n".format(num,f[0],f[1]))
-        FILES.append(filename)
+def get_files():
+    files = []
+    for filename in os.listdir():
+        f = os.path.splitext(filename) # split name and extension
+        num = f[0][0:2] # check order: 01 (01_Name.lnk)
+        if f[1] == '.lnk':
+            # print("order: '{0}', name: '{1}', ext: '{2}' \n".format(num,f[0],f[1]))
+            files.append(filename)
 
-print("\nFILES: ",FILES,"\n")
+    return files
 
-#Check what directory we are in
-# print("\ndirectory:",os.path.abspath)
+def create_pdfs(filelist):
+    # Create a new PdfFileWriter object which represents a blank PDF document
+    pdfWriter = PyPDF2.PdfFileWriter()
 
-# Create a new PdfFileWriter object which represents a blank PDF document
-pdfWriter = PyPDF2.PdfFileWriter()
+    for file in filelist:
+        print("■ Processing file: ",file)
+        file_link = __readLink(file)
+        # print("\nReal file is: ",file_link)
+        try:
+            # Read the file opened
+            file_reader = PyPDF2.PdfFileReader(file_link)
 
-for file in FILES:
-    print("■ Processing file: ",file)
-    file_link = __readLink(file)
-    print("\nReal file is: ",file_link)
-    # Read the file opened
-    file_reader = PyPDF2.PdfFileReader(file_link)
-    # Loop through all the pagenumbers for the current document
-    for pageNum in range(file_reader.numPages):
-        pageObj = file_reader.getPage(pageNum)
-        pdfWriter.addPage(pageObj)
-    print("\nFile added\n\n")
+            # Loop through all the pagenumbers for the current document
+            for pageNum in range(file_reader.numPages):
+                pageObj = file_reader.getPage(pageNum)
+                pdfWriter.addPage(pageObj)
+            print(" File added\n")
 
-# Write all the pages copied to a new final document
-pdfOutputFile = open(OUTPUT, 'wb')
-pdfWriter.write(pdfOutputFile)
+        except PyPDF2.utils.PdfReadError as e:
+            print(" Error '{0}' while processing: '{1}'\n".format(e, file))
 
-# Close file and open it using default app
-pdfOutputFile.close()
-print("■■■■ Opening resulting file... ■■■■\n\n")
-os.startfile(OUTPUT)
-# file1.close()
-# file2.close()
+    # Write all the pages copied to a new final document
+    pdfOutputFile = open(OUTPUT, 'wb')
+    pdfWriter.write(pdfOutputFile)
+
+    # Save file and open it using default app
+    pdfOutputFile.close()
+    print("■■■■ Opening resulting file... ■■■■\n\n")
+    os.startfile(OUTPUT)
+
+def main():
+    files = get_files()
+    print("\n■■■■ Files to process: \n\n",files,"\n\n■■■■\n")
+    create_pdfs(files)
+
+if __name__ == "__main__":
+    main()
+
+
